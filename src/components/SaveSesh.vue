d094c669fce502bd56c74cb07ed4d4636303ab45
Henrik <henrik.johansson@vessinge.se>
2019-04-16 13:14:04 +0200

    added some stuff

---
+++ src/components/SaveSesh.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="saveboi" align="right">
+        <p>bla bla</p>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        msg: String
+    },
+    render: function(createElement) {
+
+        return createElement('p', this.msg)
+    }
+}
+</script>
