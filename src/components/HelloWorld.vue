fa0bca1688d6db2e04dcc85408ec877f551bced5
Henrik <henrik.johansson@vessinge.se>
2019-05-01 16:54:06 +0200

    Trouble

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,10 +7,10 @@
 </template>
 <script>
 export default {
-    name: 'HelloWorld',
-    props: {
-        msg: String,
-    },
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
